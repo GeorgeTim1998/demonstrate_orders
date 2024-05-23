4c78f5036037691392606cec581ff10b61c34dc2
GeorgeTim1998 <georgetim1998@gmail.com>
2024-05-24 00:31:53 +0400

    add publisher

---
+++ cmd/publisher/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/nats-io/nats.go"
+)
+
+func main() {
+	nc, err := nats.Connect("nats://localhost:4223")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer nc.Close()
+
+	subject := "test.subject"
+	msg := []byte("Message")
+	err = nc.Publish(subject, msg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Published message:", string(msg))
+
+	time.Sleep(time.Second)
+}
