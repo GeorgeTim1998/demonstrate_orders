708b7581d6db15e98e5d5c683df6c3d77508edb3
GeorgeTim1998 <georgetim1998@gmail.com>
2024-05-26 23:54:58 +0400

    save orders to DB

--- cmd/receiver/main.go
+++ cmd/receiver/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"demonstrate_orders/config"
+	DB "demonstrate_orders/db"
+	"encoding/json"
 	"log"
+	"sync"
+
+	"demonstrate_orders/db/models"
 
 	"github.com/nats-io/stan.go"
+)
+
+var (
+	cache = make(map[string]models.Order)
+	mu    sync.Mutex
 )
 
 func main() {
@@ -14,12 +25,92 @@ func main() {
 	defer sc.Close()
 
 	subscription, err := sc.Subscribe("test.subject", func(msg *stan.Msg) {
-		log.Printf("Received message: %s", string(msg.Data))
-		log.Printf("Message Body Twice: %s %s", string(msg.Data), string(msg.Data))
-		msg.Ack()
-	}, stan.DurableName("my-durable"))
+		var order models.Order
+		err := json.Unmarshal(msg.Data, &order)
+		if err != nil {
+			log.Printf("Error parsing message: %v", err)
+			return
+		}
+
+		mu.Lock()
+		cache[order.OrderUID] = order
+		mu.Unlock()
+
+		log.Printf("Received message: %s", order.OrderUID)
+
+		err = saveOrderToDB(order)
+		if err != nil {
+			log.Printf("Error saving order to database: %v", err)
+		} else {
+			log.Printf("Saved to the DB: %s", order.OrderUID)
+			msg.Ack()
+		}
+	}, stan.DurableName("my-durable"), stan.SetManualAckMode())
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer subscription.Close()
+
+	select {}
+}
+
+func saveOrderToDB(order models.Order) error {
+	config, err := config.LoadConfig()
+	if err != nil {
+		log.Printf("Error loading the config: %v", err)
+		return err
+	}
+
+	db, err := DB.InitDB(config)
+	if err != nil {
+		log.Printf("Error init DB: %v", err)
+		return err
+	}
+	defer db.Close()
+
+	tx, err := db.Begin()
+	if err != nil {
+		log.Printf("Error beginning the connection to the db: %v", err)
+		return err
+	}
+
+	_, err = tx.Exec(`
+		INSERT INTO orders (order_uid, track_number, entry, locale, internal_signature, customer_id, delivery_service, shardkey, sm_id, date_created, oof_shard)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`,
+		order.OrderUID, order.TrackNumber, order.Entry, order.Locale, order.InternalSignature, order.CustomerID, order.DeliveryService, order.Shardkey, order.SmID, order.DateCreated, order.OofShard)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	_, err = tx.Exec(`
+		INSERT INTO deliveries (order_uid, name, phone, zip, city, address, region, email)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
+		order.OrderUID, order.Delivery.Name, order.Delivery.Phone, order.Delivery.Zip, order.Delivery.City, order.Delivery.Address, order.Delivery.Region, order.Delivery.Email)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	_, err = tx.Exec(`
+		INSERT INTO payments (transaction, order_uid, currency, provider, amount, payment_dt, bank, delivery_cost, goods_total, custom_fee)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`,
+		order.Payment.Transaction, order.OrderUID, order.Payment.Currency, order.Payment.Provider, order.Payment.Amount, order.Payment.PaymentDt, order.Payment.Bank, order.Payment.DeliveryCost, order.Payment.GoodsTotal, order.Payment.CustomFee)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	for _, item := range order.Items {
+		_, err = tx.Exec(`
+			INSERT INTO items (chrt_id, order_uid, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+			VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`,
+			item.ChrtID, order.OrderUID, item.TrackNumber, item.Price, item.Rid, item.Name, item.Sale, item.Size, item.TotalPrice, item.NmID, item.Brand, item.Status)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+
+	return tx.Commit()
 }
