0e82a2a8fa1a7f964ba87a016394b31a8ded27d0
GeorgeTim1998 <georgetim1998@gmail.com>
2024-05-26 23:34:49 +0400

    add order_uid as its own foreign key field

--- db/migrations/20240525190217_add_db_tables.sql
+++ db/migrations/20240525190217_add_db_tables.sql
@@ -16,19 +16,22 @@ CREATE TABLE IF NOT EXISTS "orders" (
 
 -- Миграция для создания таблицы "Delivery"
 CREATE TABLE IF NOT EXISTS "deliveries" (
-    "order_uid" VARCHAR(255) PRIMARY KEY,
+    "id" SERIAL PRIMARY KEY,
+    "order_uid" VARCHAR(255),
     "name" VARCHAR(255),
     "phone" VARCHAR(255),
     "zip" VARCHAR(255),
     "city" VARCHAR(255),
     "address" VARCHAR(255),
     "region" VARCHAR(255),
-    "email" VARCHAR(255)
+    "email" VARCHAR(255),
+    FOREIGN KEY ("order_uid") REFERENCES "orders" ("order_uid")
 );
 
 -- Миграция для создания таблицы "payments"
 CREATE TABLE IF NOT EXISTS "payments" (
-    "order_uid" VARCHAR(255) PRIMARY KEY,
+    "id" SERIAL PRIMARY KEY,
+    "order_uid" VARCHAR(255),
     "transaction" VARCHAR(255),
     "request_id" VARCHAR(255),
     "currency" VARCHAR(255),
@@ -38,12 +41,14 @@ CREATE TABLE IF NOT EXISTS "payments" (
     "bank" VARCHAR(255),
     "delivery_cost" INTEGER,
     "goods_total" INTEGER,
-    "custom_fee" INTEGER
+    "custom_fee" INTEGER,
+    FOREIGN KEY ("order_uid") REFERENCES "orders" ("order_uid")
 );
 
 -- Миграция для создания таблицы "items"
 CREATE TABLE IF NOT EXISTS "items" (
-    "chrt_id" INTEGER PRIMARY KEY,
+    "id" SERIAL PRIMARY KEY,
+    "chrt_id" INTEGER,
     "order_uid" VARCHAR(255),
     "track_number" VARCHAR(255),
     "price" INTEGER,
@@ -54,22 +59,18 @@ CREATE TABLE IF NOT EXISTS "items" (
     "total_price" INTEGER,
     "nm_id" INTEGER,
     "brand" VARCHAR(255),
-    "status" INTEGER
+    "status" INTEGER,
+    FOREIGN KEY ("order_uid") REFERENCES "orders" ("order_uid")
 );
-
--- Создание внешних ключей
-ALTER TABLE "deliveries" ADD FOREIGN KEY ("order_uid") REFERENCES "orders" ("order_uid");
-ALTER TABLE "payments" ADD FOREIGN KEY ("order_uid") REFERENCES "orders" ("order_uid");
-ALTER TABLE "items" ADD FOREIGN KEY ("order_uid") REFERENCES "orders" ("order_uid");
 
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 -- Удаление внешних ключей
-ALTER TABLE "items" DROP CONSTRAINT "item_order_uid_fkey";
-ALTER TABLE "payments" DROP CONSTRAINT "payment_order_uid_fkey";
-ALTER TABLE "deliveries" DROP CONSTRAINT "delivery_order_uid_fkey";
+ALTER TABLE "items" DROP CONSTRAINT IF EXISTS "items_order_uid_fkey";
+ALTER TABLE "payments" DROP CONSTRAINT IF EXISTS "payments_order_uid_fkey";
+ALTER TABLE "deliveries" DROP CONSTRAINT IF EXISTS "deliveries_order_uid_fkey";
 
 -- Удаление таблиц
 DROP TABLE IF EXISTS "items";
