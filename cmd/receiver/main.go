17065f2fd8ad664d78be5ab21637915499936d51
GeorgeTim1998 <georgetim1998@gmail.com>
2024-05-27 12:46:17 +0400

    optimize insert operations

--- cmd/receiver/main.go
+++ cmd/receiver/main.go
@@ -74,43 +74,78 @@ func saveOrderToDB(order models.Order) error {
 		return err
 	}
 
-	_, err = tx.Exec(`
-		INSERT INTO orders (order_uid, track_number, entry, locale, internal_signature, customer_id, delivery_service, shardkey, sm_id, date_created, oof_shard)
-		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`,
-		order.OrderUID, order.TrackNumber, order.Entry, order.Locale, order.InternalSignature, order.CustomerID, order.DeliveryService, order.Shardkey, order.SmID, order.DateCreated, order.OofShard)
+	ordersStmt, err := tx.Prepare(`
+	INSERT INTO orders (order_uid, track_number, entry, locale, internal_signature, customer_id, delivery_service, shardkey, sm_id, date_created, oof_shard)
+	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`)
 	if err != nil {
-		tx.Rollback()
+		log.Printf("Error preparing statement for orders table: %v", err)
+		return err
+	}
+	defer ordersStmt.Close()
+
+	_, err = ordersStmt.Exec(order.OrderUID, order.TrackNumber, order.Entry, order.Locale, order.InternalSignature, order.CustomerID, order.DeliveryService, order.Shardkey, order.SmID, order.DateCreated, order.OofShard)
+	if err != nil {
+		log.Printf("Error inserting data into orders table: %v", err)
 		return err
 	}
 
-	_, err = tx.Exec(`
-		INSERT INTO deliveries (order_uid, name, phone, zip, city, address, region, email)
-		VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
-		order.OrderUID, order.Delivery.Name, order.Delivery.Phone, order.Delivery.Zip, order.Delivery.City, order.Delivery.Address, order.Delivery.Region, order.Delivery.Email)
+	// Подготовленный запрос для вставки в таблицу "deliveries"
+	deliveriesStmt, err := tx.Prepare(`
+	INSERT INTO deliveries (order_uid, name, phone, zip, city, address, region, email)
+	VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`)
 	if err != nil {
-		tx.Rollback()
+		log.Printf("Error preparing statement for deliveries table: %v", err)
+		return err
+	}
+	defer deliveriesStmt.Close()
+
+	// Выполнение запроса для вставки в таблицу "deliveries"
+	_, err = deliveriesStmt.Exec(order.OrderUID, order.Delivery.Name, order.Delivery.Phone, order.Delivery.Zip, order.Delivery.City, order.Delivery.Address, order.Delivery.Region, order.Delivery.Email)
+	if err != nil {
+		log.Printf("Error inserting data into deliveries table: %v", err)
 		return err
 	}
 
-	_, err = tx.Exec(`
-		INSERT INTO payments (transaction, order_uid, currency, provider, amount, payment_dt, bank, delivery_cost, goods_total, custom_fee)
-		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`,
-		order.Payment.Transaction, order.OrderUID, order.Payment.Currency, order.Payment.Provider, order.Payment.Amount, order.Payment.PaymentDt, order.Payment.Bank, order.Payment.DeliveryCost, order.Payment.GoodsTotal, order.Payment.CustomFee)
+	// Подготовленный запрос для вставки в таблицу "payments"
+	paymentsStmt, err := tx.Prepare(`
+	INSERT INTO payments (transaction, order_uid, currency, provider, amount, payment_dt, bank, delivery_cost, goods_total, custom_fee)
+	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`)
 	if err != nil {
-		tx.Rollback()
+		log.Printf("Error preparing statement for payments table: %v", err)
+		return err
+	}
+	defer paymentsStmt.Close()
+
+	_, err = paymentsStmt.Exec(order.Payment.Transaction, order.OrderUID, order.Payment.Currency, order.Payment.Provider, order.Payment.Amount, order.Payment.PaymentDt, order.Payment.Bank, order.Payment.DeliveryCost, order.Payment.GoodsTotal, order.Payment.CustomFee)
+	if err != nil {
+		log.Printf("Error inserting data into payments table: %v", err)
 		return err
 	}
 
+	// Подготовленный запрос для вставки в таблицу "items"
+	itemsStmt, err := tx.Prepare(`
+	INSERT INTO items (chrt_id, order_uid, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
+	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`)
+	if err != nil {
+		log.Printf("Error preparing statement for items table: %v", err)
+		return err
+	}
+	defer itemsStmt.Close()
+
+	// Выполнение запроса для вставки в таблицу "items" для каждого элемента заказа
 	for _, item := range order.Items {
-		_, err = tx.Exec(`
-			INSERT INTO items (chrt_id, order_uid, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
-			VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`,
-			item.ChrtID, order.OrderUID, item.TrackNumber, item.Price, item.Rid, item.Name, item.Sale, item.Size, item.TotalPrice, item.NmID, item.Brand, item.Status)
+		_, err = itemsStmt.Exec(item.ChrtID, order.OrderUID, item.TrackNumber, item.Price, item.Rid, item.Name, item.Sale, item.Size, item.TotalPrice, item.NmID, item.Brand, item.Status)
 		if err != nil {
-			tx.Rollback()
+			log.Printf("Error inserting data into items table: %v", err)
 			return err
 		}
 	}
 
-	return tx.Commit()
+	if err := tx.Commit(); err != nil {
+		tx.Rollback()
+		log.Printf("Error committing transaction: %v", err)
+		return err
+	}
+
+	return nil
 }
