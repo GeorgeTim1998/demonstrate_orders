a4a49628298512385a049a4a959346bd87efc770
GeorgeTim1998 <georgetim1998@gmail.com>
2024-05-27 15:25:50 +0400

    add server

---
+++ cmd/server/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"demonstrate_orders/global"
+
+	"github.com/gorilla/mux"
+)
+
+func getOrderHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	orderUID := vars["orderUID"]
+
+	global.Mu.Lock()
+	order, exists := global.Cache[orderUID]
+	global.Mu.Unlock()
+
+	if !exists {
+		http.Error(w, "Order not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(order)
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/order/{orderUID}", getOrderHandler).Methods("GET")
+
+	http.Handle("/", r)
+	log.Println("Server is running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
