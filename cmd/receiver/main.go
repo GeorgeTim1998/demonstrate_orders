9ee3ba04b67088d0b940a186d1c12af8b6b8bc62
GeorgeTim1998 <georgetim1998@gmail.com>
2024-05-28 12:24:50 +0400

    remove comments

--- cmd/receiver/main.go
+++ cmd/receiver/main.go
@@ -86,7 +86,6 @@ func saveOrderToDB(order models.Order) error {
 		return err
 	}
 
-	// Подготовленный запрос для вставки в таблицу "deliveries"
 	deliveriesStmt, err := tx.Prepare(`
 	INSERT INTO deliveries (order_uid, name, phone, zip, city, address, region, email)
 	VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`)
@@ -96,14 +95,12 @@ func saveOrderToDB(order models.Order) error {
 	}
 	defer deliveriesStmt.Close()
 
-	// Выполнение запроса для вставки в таблицу "deliveries"
 	_, err = deliveriesStmt.Exec(order.OrderUID, order.Delivery.Name, order.Delivery.Phone, order.Delivery.Zip, order.Delivery.City, order.Delivery.Address, order.Delivery.Region, order.Delivery.Email)
 	if err != nil {
 		log.Printf("Error inserting data into deliveries table: %v", err)
 		return err
 	}
 
-	// Подготовленный запрос для вставки в таблицу "payments"
 	paymentsStmt, err := tx.Prepare(`
 	INSERT INTO payments (transaction, order_uid, currency, provider, amount, payment_dt, bank, delivery_cost, goods_total, custom_fee)
 	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`)
@@ -119,7 +116,6 @@ func saveOrderToDB(order models.Order) error {
 		return err
 	}
 
-	// Подготовленный запрос для вставки в таблицу "items"
 	itemsStmt, err := tx.Prepare(`
 	INSERT INTO items (chrt_id, order_uid, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status)
 	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`)
@@ -129,7 +125,6 @@ func saveOrderToDB(order models.Order) error {
 	}
 	defer itemsStmt.Close()
 
-	// Выполнение запроса для вставки в таблицу "items" для каждого элемента заказа
 	for _, item := range order.Items {
 		_, err = itemsStmt.Exec(item.ChrtID, order.OrderUID, item.TrackNumber, item.Price, item.Rid, item.Name, item.Sale, item.Size, item.TotalPrice, item.NmID, item.Brand, item.Status)
 		if err != nil {
--- main.go
+++ main.go
@@ -7,20 +7,17 @@ import (
 )
 
 func main() {
-	// Запуск сервера
 	go func() {
 		if err := server.Run(); err != nil {
 			log.Fatalf("Error running server: %v", err)
 		}
 	}()
 
-	// Запуск получателя сообщений
 	go func() {
 		if err := receiver.Run(); err != nil {
 			log.Fatalf("Error running receiver: %v", err)
 		}
 	}()
 
-	// Ожидание завершения работы приложения
 	select {}
 }
